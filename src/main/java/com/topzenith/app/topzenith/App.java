c67cf508dea2f38580d646ae63285ec2f46f8907
swamyrajashekar <swamyrajashekar@gmail.com>
2024-09-19 21:27:42 +0530

    first commit

---
+++ src/main/java/com/topzenith/app/topzenith/App.java
@@ -0,0 +1,10 @@
+package com.topzenith.app.topzenith;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
